8ffed8fdedc5a4db77cca8f4dffb79dd75a0a3ad
Brennan Heisler <learn@heislercreative.com>
2018-01-23 02:52:25 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,11 @@
+# def square_array(array)
+#   square = []
+#   array.each { |number| square.push(number ** 2) }
+#   square
+# end
+
+
 def square_array(array)
-  square = []
-  array.each { |number| square.push(number ** 2) }
-  square 
+  square = array.collect { |number| square.push(number ** 2) }
+  square
 end
